451160d7832411d528b3daaf2956bf6e30fb6ef4
Saimon Eccher <eccher.saimon11@gmail.com>
2025-09-22 16:36:57 +0200

    Little fix

--- code.R
+++ code.R
@@ -9,12 +9,19 @@ library(dplyr)
 setwd("C:\\Users\\ecche\\Desktop\\Tesi\\HFCS\\HFCS2014\\dati_wave2_csv")
 
 # Load data
-data <- read.csv("d1.csv", sep = ",", header = TRUE)
-repl_weights <- read.csv("w.csv",sep=",",header = TRUE) 
+data_der <- read.csv("d1.csv", sep = ",", header = TRUE)
+
+repl_weights <- read.csv("w.csv",sep=",",header = TRUE)
+
+data_core1 <- read.csv("p1.csv",sep = ",", header = TRUE)
+data_core2 <- read.csv("h1.csv",sep = ",",header=TRUE)
+
+data_noncore2 <- read.csv("hn1.csv",sep = ",",header=TRUE)
+data_noncore1 <- read.csv("pn1.csv",sep = ",", header = TRUE)
 
 
 # Rename variables 
-data <- data %>%
+data_der <- data_der %>%
   rename(ws = HW0010)
 
 repl_weights <- repl_weights%>%
